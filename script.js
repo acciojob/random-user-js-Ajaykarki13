5070cff00578e7765b8c9686ae6b2246158cbf81
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-02-19 17:52:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,61 @@
 //your code here
+let main=document.querySelector("main");
+
+let getUser=()=>{
+    fetch("https://randomuser.me/api/")
+    .then((res)=>res.json())
+    .then((data)=>displayUser(data.results[0]))
+    .catch((err)=>console.log(err));
+}
+getUser();
+
+let displayUser = (data)=>{
+    console.log(data);
+    main.innerHTML="";
+    let userDiv=document.createElement("div");
+    let img= document.createElement("img");
+    img.src=data.picture.large;
+    let name=document.createElement("p");
+    name.textContent=`${data.name.first} ${data.name.last}`;
+    let additionalInfo=document.createElement("p");
+    additionalInfo.setAttribute("heading","Additional Info");
+    let age=document.createElement("button");
+    age.textContent="Age";
+    age.setAttribute("data-attr","age");
+    let email=document.createElement("button");
+    email.textContent="Email";
+    email.setAttribute("data-attr","email")
+    let phone=document.createElement("button");
+    phone.textContent="Phone";
+    phone.setAttribute("data-attr","phone")
+    let anotherUser=document.createElement("button");
+    anotherUser.setAttribute("id","getUser");
+    anotherUser.textContent="GET ANOTHER USER";
+
+    userDiv.append(img,name,additionalInfo,age,email,phone,document.createElement("br"),anotherUser);
+    main.append(userDiv);
+
+    age.addEventListener("click",()=>{
+        additionalInfo.innerHTML=data.dob.age;
+    })
+
+    email.addEventListener("click",()=>{
+        additionalInfo.innerHTML=data.email;
+    })
+
+    phone.addEventListener("click",()=>{
+        additionalInfo.innerHTML=data.phone;
+    })
+
+    anotherUser.addEventListener("click",()=>{
+        getUser();
+    })
+}
+
+
+
+
+/*
 let btn1 =  document.querySelector("#one")
 let btn2 =  document.querySelector("#two")
 let btn3 =  document.querySelector("#three")
@@ -59,4 +116,4 @@ btn3.addEventListener("click",()=>{document.querySelector("#c").style.display="b
 })
 })
   })
-
+*/
